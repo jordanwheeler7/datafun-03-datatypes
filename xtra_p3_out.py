ae1f15235f8bdda5f17dba45eaf08b319a75c4b2
jordanwheeler7 <122232837+jordanwheeler7@users.noreply.github.com>
2023-01-29 22:06:15 -0500

    Project Complete

---
+++ xtra_p3_out.py
@@ -0,0 +1,14 @@
+"""
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-03-datatypes> & C:/Users/jorda/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/jorda/OneDrive/Desktop/Northwest Homework/DATA ANALYTICS FUNDAMENTALS/datafun-03-datatypes/xtra_p3.py"
+Number of words with over 10 letters in Hamlet 415
+Number of words with over 10 letters in Julius Caesar 197
+Long words in both 13
+
+Similar long words in both sorted(longwords) = ['conference.', 'conscience;', 'consequence;', 'constantly.', 'corruption,', 'countenance', 'countenance,', 'enterprise,', 'honourable.', 'immediately.', 'replication', 'themselves,', 'threatening']
+
+TESTING...if nothing prints before the testing is done, you passed!
+TESTING DONE
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-03-datatypes> 
+    
+    
+"""
