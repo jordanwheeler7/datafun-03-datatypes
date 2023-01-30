325c95970d2a02a6879877d1ac4579675b37da5b
jordanwheeler7 <122232837+jordanwheeler7@users.noreply.github.com>
2023-01-29 21:36:52 -0500

    Project Completion

---
+++ user_tuples_etc_out.py
@@ -0,0 +1,49 @@
+"""
+Jordan Wheeler
+29 Jan 2023
+Project 3
+
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-03-datatypes> & C:/Users/jorda/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/jorda/OneDrive/Desktop/Northwest Homework/DATA ANALYTICS FUNDAMENTALS/datafun-03-datatypes/user_tuples_etc.py"
+
+Practice Tuples
+
+
+Workout Measurement is: {workout_measure}
+Mile 1 time was: {(9,)}
+Mile 2 time was: {(9,)}
+Mile 3 tiem was: {(7,)}
+Double Mile 2: {(9, 9)}
+9 is in wourkout_1_splits {True}
+11 is in workout_2_splits {False}
+
+The first workout split was 9.
+The last workout split was 8.
+
+workout_1_splits = (9, 10, 11, 8)
+workout_2_splits = (8, 9, 10, 13)
+workout_3_splits = (6, 9, 11, 7)
+
+================================================================
+
+Practice with Sets
+
+Set 1: ('Dog', 'Cat', 'Bird', 'Snake', 'Iguana')
+Set 2: ('Bear', 'Wolf', 'Cat', 'Dog', 'Spider')
+
+
+The union of set 1 and 2 is: {'Dog', 'Cat', ('Dog', 'Cat', 'Bird', 'Snake', 'Iguana'), 'Wolf', 'Spider', 'Bear'}
+The intersection of set 1 and set 2 is: set()
+
+['Dog', 'Wolf', 'Cat', ('Dog', 'Cat', 'Bird', 'Snake', 'Iguana'), 'Spider', 'Bear']
+
+================================================================
+
+Practicing with Dictionaries
+
+{'name': ['Bob', 'Erin', 'Tom', 'Paco'], 'age': [1, 3, 5, 7], 'weight': [12, 15, 19, 21]}    
+
+{'Beautiful': 1, 'is': 10, 'better': 8, 'than': 8, 'ugly.': 1, 'Explicit': 1, 'implicit.': 1, 'Simple': 1, 'complex.': 1, 'Complex': 1, 'complicated.': 1, 'Flat': 1, 'nested.': 1, 'Sparse': 1, 'dense.': 1, 'Readability': 1, 'counts.': 1, 'Special': 1, 'cases': 1, "aren't": 1, 'special': 1, 'enough': 1, 'to': 5, 'break': 1, 'the': 5, 'rules.': 1, 'Although': 3, 'practicality': 1, 'beats': 1, 'purity.': 1, 'Errors': 1, 'should': 2, 'never': 2, 'pass': 1, 'silently.': 1, 'Unless': 1, 'explicitly': 1, 'silenced.': 1, 'In': 1, 'face': 1, 'of': 2, 'ambiguity,': 1, 'refuse': 1, 'temptation': 1, 'guess.': 1, 'There': 1, 'be': 3, 'one--': 1, 'and': 1, 'preferably': 1, 'only': 1, 'one': 2, '--obvious': 1, 'way': 2, 'do': 2, 'it.': 1, 'that': 1, 'may': 2, 'not': 1, 'obvious': 1, 'at': 1, 'first': 1, 'unless': 1, "you're": 1, 'Dutch.': 1, 'Now': 1, 'never.': 1, 'often': 1, '*right*': 1, 'now.': 1, 'If': 2, 'implementation': 2, 'hard': 1, 'explain,': 2, "it's": 1, 'a': 2, 'bad': 1, 'idea.': 2, 'easy': 1, 'it': 1, 'good': 1, 'Namespaces': 1, 'are': 1, 'honking': 1, 'great': 1, 'idea': 1, '--': 1, "let's": 1, 'more': 1, 'those!': 1}
+
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\datafun-03-datatypes>
+
+"""
